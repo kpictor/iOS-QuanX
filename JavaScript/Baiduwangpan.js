761535169d9a09d8667eff9c6f8c4cca32a5139e
kpictor <26703096+kpictor@users.noreply.github.com>
2019-11-24 02:04:22 +0800

    baiducloud up date

--- JavaScript/Baiduwangpan.js
+++ JavaScript/Baiduwangpan.js
@@ -17,7 +17,7 @@ obj = {
     "buy_time": 0,
     "product_id": "1",
     "auto_upgrade_to_svip": 0,
-    "end_time": 1672360448,
+    "end_time": 1953216000,
     "cluster": "vip",
     "detail_cluster": "svip",
     "status": 0
